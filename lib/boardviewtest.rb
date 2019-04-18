c4cf76891407497bbee5097f8fe088b8a19037bc
Jean-Edouard Fages <jeanedouard.fages@gmail.com>
2019-04-18 11:25:01 +0200

    first commit

---
+++ lib/boardviewtest.rb
@@ -0,0 +1,23 @@
+system("clear")
+p_hash = { :a1 => " ", :a2 => "O", :a3 => "X", :b1 => "X", :b2 => "X", :b3 => "X", :c1 => "O", :c2 => "X", :c3 => "X" }
+puts p_hash[:a2]
+
+
+puts <<-'EOF'
+       _____ _____ _____ 
+      |     |     |     |
+EOF
+puts "      |  #{p_hash[:a1]}  |  #{p_hash[:a2]}  |  #{p_hash[:a3]}  |      "
+puts <<-'EOF'
+      |_____|_____|_____|
+      |     |     |     |
+EOF
+puts "      |  #{p_hash[:b1]}  |  #{p_hash[:b2]}  |  #{p_hash[:b3]}  |      "
+puts <<-'EOF'
+      |_____|_____|_____|
+      |     |     |     |
+EOF
+puts "      |  #{p_hash[:c1]}  |  #{p_hash[:c2]}  |  #{p_hash[:c3]}  |      "
+puts <<-'EOF'
+      |_____|_____|_____|
+EOF
